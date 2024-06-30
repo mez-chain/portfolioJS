9d4bb487370b59f47f5f25f023e6c9cb47148382
N.MEZIANY <mez.blockchain@gmail.com>
2024-06-30 22:41:57 +0300

    added code to make the navbar responsive

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,5 +1,23 @@
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll(".nav-link");
+  const toggleButtonBars = document.getElementById("toggle-button-bars");
+  const toggleButtonX = document.getElementById("toggle-button-x");
+  const navbar = document.getElementById("navbar");
+
+  toggleButtonX.classList.add("button-d-none");
+
+  toggleButtonBars.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    toggleButtonBars.classList.toggle("button-display-bars");
+    toggleButtonX.classList.toggle("button-display-x");
+  });
+
+  toggleButtonX.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+    toggleButtonX.classList.toggle("button-display-x");
+    toggleButtonBars.classList.toggle("button-display-bars");
+  });
+
 
   function setActiveLink() {
     const hash = window.location.hash;
